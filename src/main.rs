78a87be0156e9ae8fe90dff5f8e2157595119c81
Sashank Gogula <sashank.gogula@glean.com>
2024-02-21 21:46:12 -0800

    rm server deps from tailscale

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::sync::Arc;
+
+use hyper_util::{
+    rt::{TokioExecutor, TokioIo},
+    service::TowerToHyperService,
+};
 use tailscale::{TSNetwork, Tailscale};
 
 mod graph;
@@ -5,7 +11,7 @@ mod router;
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<(), anyhow::Error> {
-    let ts = Tailscale::new();
+    let ts = Arc::new(Tailscale::new());
     ts.up().await?;
     println!("Tailscale is up!");
 
@@ -14,7 +20,19 @@ async fn main() -> Result<(), anyhow::Error> {
     // build our application with a single route
     let app = router::new_router(conn);
 
-    ts.listen(TSNetwork::TCP, ":3000", app).await?;
+    let listener = ts.listen(TSNetwork::TCP, ":3000")?;
+    loop {
+        let conn = ts.accept(listener).await?;
 
-    Ok(())
+        let service = app.clone();
+        tokio::task::spawn(async move {
+            // hyper::server::conn::http2::Builder::new(TokioExecutor::new())
+            hyper::server::conn::http1::Builder::new()
+                .serve_connection(
+                    TokioIo::new(tokio::net::TcpStream::from_std(conn).unwrap()),
+                    TowerToHyperService::new(service),
+                )
+                .await
+        });
+    }
 }
--- tailscale/src/lib.rs
+++ tailscale/src/lib.rs
@@ -5,15 +5,10 @@
 use std::{
     ffi::{c_int, CStr, CString},
     fs::OpenOptions,
+    net::TcpStream,
     os::fd::{AsRawFd, FromRawFd},
+    sync::Arc,
 };
-
-use hyper::server::conn::http2::Builder;
-use hyper_util::{
-    rt::{TokioExecutor, TokioIo},
-    service::TowerToHyperService,
-};
-use tokio::task::spawn_blocking;
 
 include!(concat!(env!("OUT_DIR"), "/libtailscale.rs"));
 
@@ -31,9 +26,13 @@ impl ToString for TSNetwork {
     }
 }
 
-#[derive(Clone)]
+#[derive(Clone, Copy)]
+pub struct TailscaleListener {
+    inner: i32,
+}
+
 pub struct Tailscale {
-    ts: tailscale,
+    inner: tailscale,
 }
 
 impl Tailscale {
@@ -47,7 +46,7 @@ impl Tailscale {
                 .expect("failed to open log file");
             let fd = file.as_raw_fd();
             tailscale_set_logfd(ts, fd);
-            Tailscale { ts }
+            Tailscale { inner: ts }
         }
     }
 
@@ -69,68 +68,58 @@ impl Tailscale {
             tailscale_set_authkey(ts, auth_key.as_ptr());
             tailscale_set_control_url(ts, control_url.as_ptr());
             tailscale_set_ephemeral(ts, ephemeral.into());
-            Tailscale { ts }
+            Tailscale { inner: ts }
         }
     }
 
-    pub async fn up(&self) -> anyhow::Result<()> {
-        let ts = self.ts;
-        tokio::task::spawn_blocking(move || handle_error(ts, unsafe { tailscale_up(ts) })).await?
+    pub async fn up(self: &Arc<Self>) -> anyhow::Result<()> {
+        let ts = self.clone();
+        tokio::task::spawn_blocking(move || ts.handle_error(unsafe { tailscale_up(ts.inner) }))
+            .await?
     }
 
-    pub async fn listen(
-        &self,
-        network: TSNetwork,
-        addr: &str,
-        service: axum::Router,
-    ) -> anyhow::Result<()> {
+    pub fn listen(&self, network: TSNetwork, addr: &str) -> anyhow::Result<TailscaleListener> {
         let network = CString::new(network.to_string()).unwrap();
         let addr = CString::new(addr).unwrap();
         let mut listener = 0;
-        handle_error(self.ts, unsafe {
-            tailscale_listen(self.ts, network.as_ptr(), addr.as_ptr(), &mut listener)
+        self.handle_error(unsafe {
+            tailscale_listen(self.inner, network.as_ptr(), addr.as_ptr(), &mut listener)
         })?;
-        println!("{:?}", listener);
+        println!("Listening on {:?}", listener);
+        Ok(TailscaleListener { inner: listener })
+    }
 
-        loop {
-            let conn_out = {
-                let ts = self.ts;
-                spawn_blocking(move || {
-                    let mut conn_out = 0;
-                    handle_error(ts, unsafe { tailscale_accept(listener, &mut conn_out) })?;
-                    Ok::<i32, anyhow::Error>(conn_out)
-                })
-                .await?
-            }?;
+    pub async fn accept(
+        self: &Arc<Self>,
+        listener: TailscaleListener,
+    ) -> anyhow::Result<TcpStream> {
+        let ts = self.clone();
+        let conn = tokio::task::spawn_blocking(move || {
+            let mut conn_out = 0;
+            ts.handle_error(unsafe { tailscale_accept(listener.inner, &mut conn_out) })?;
+            anyhow::Ok(conn_out)
+        })
+        .await??;
+        let tcp_stream = unsafe { std::net::TcpStream::from_raw_fd(conn) };
+        tcp_stream.set_nonblocking(true)?;
+        Ok(tcp_stream)
+    }
 
-            let tcp_stream = unsafe { std::net::TcpStream::from_raw_fd(conn_out) };
-            tcp_stream.set_nonblocking(true)?;
-            let tcp_stream = TokioIo::new(tokio::net::TcpStream::from_std(tcp_stream)?);
-
-            let service = service.clone();
-            tokio::task::spawn(async move {
-                Builder::new(TokioExecutor::new())
-                    .serve_connection(tcp_stream, TowerToHyperService::new(service))
-                    .await
-            });
+    fn handle_error(&self, code: c_int) -> anyhow::Result<()> {
+        if code != -1 {
+            return Ok(());
         }
+        let mut buffer: [i8; 256] = [0; 256];
+        let ret = unsafe { tailscale_errmsg(self.inner, &mut buffer as *mut _, buffer.len()) };
+        if ret != 0 {
+            return Err(anyhow::anyhow!(
+                "tailscale internal error: failed to get error message"
+            ));
+        }
+        println!("{:?}", buffer);
+        let cstr = unsafe { CStr::from_ptr(buffer.as_ptr()) };
+        Err(anyhow::anyhow!(cstr.to_string_lossy()))
     }
-}
-
-fn handle_error(ts: tailscale, code: c_int) -> anyhow::Result<()> {
-    if code != -1 {
-        return Ok(());
-    }
-    let mut buffer: [i8; 256] = [0; 256];
-    let ret = unsafe { tailscale_errmsg(ts, &mut buffer as *mut _, buffer.len()) };
-    if ret != 0 {
-        return Err(anyhow::anyhow!(
-            "tailscale internal error: failed to get error message"
-        ));
-    }
-    println!("{:?}", buffer);
-    let cstr = unsafe { CStr::from_ptr(buffer.as_ptr()) };
-    Err(anyhow::anyhow!(cstr.to_string_lossy()))
 }
 
 impl Default for Tailscale {
@@ -142,7 +131,7 @@ impl Default for Tailscale {
 impl Drop for Tailscale {
     fn drop(&mut self) {
         unsafe {
-            tailscale_close(self.ts);
+            tailscale_close(self.inner);
         }
     }
 }
